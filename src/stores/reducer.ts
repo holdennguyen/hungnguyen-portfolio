c483ab8494d3ac3dea304370d8503bde812b95ea
Hung. Minh Nguyen <89327966+holdennguyen@users.noreply.github.com>
2023-05-03 16:22:14 +0700

    update jenkins project

--- src/stores/reducer.ts
+++ src/stores/reducer.ts
@@ -12,6 +12,14 @@ const initState = {
   repoList: [
     {
       id: 0,
+      url: "https://github.com/holdennguyen/jenkins-setup-aws",
+      name: "Setup HA Jenkins on AWS",
+      description: "Deploy Jenkins on an AWS Autoscaling group with an Application Load Balancer and EFS filesystem for Jenkins’s data directory",
+      language: "",
+      product: "git",
+    },
+    {
+      id: 1,
       url: "https://github.com/holdennguyen/cicd-pipeline-java-webapp",
       name: "CI/CD Pipeline for Java web app",
       description: "Provisioning AWS Infrastructure by Terraform. Create Jenkins pipeline with multiple stages: Maven build, publish to Nexus, use Ansible to download artifact and deploy on Docker container",
@@ -19,7 +27,7 @@ const initState = {
       product: "git",
     },
     {
-      id: 1,
+      id: 2,
       url: "https://github.com/holdennguyen/kubernetes-install-cluster-with-kubeadm",
       name: "Kubernetes Cluster",
       description: "Bootstrapping K8s cluster with kubeadm (Provisioning by Vagrant & Virtual Box)",
@@ -27,18 +35,10 @@ const initState = {
       product: "git",
     },
     {
-      id: 2,
+      id: 3,
       url: "https://github.com/holdennguyen/kasm-workspaces-single-server",
       name: "Kasm Workspaces",
       description: "Streaming containeried apps & desktops to browser. Automation hosting Kasm Workspaces with Terraform and Linode cloud.",
-      language: "",
-      product: "git",
-    },
-    {
-      id: 3,
-      url: "https://github.com/holdennguyen/wordpress-route53-lightsail",
-      name: "Wordpress Server",
-      description: "Hosting WordPress in Amazon Lightsail & Route53 with SSL Certificates",
       language: "",
       product: "git",
     },
